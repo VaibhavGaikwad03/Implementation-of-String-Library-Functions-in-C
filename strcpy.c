a4ef9d73ff02732470290d2db873e0864926caf1
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-01-31 12:16:45 +0530

    Add files via upload

---
+++ strcpy.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+char *str_cpy(char *dest, char *src)
+{
+    while (*src != '\0')
+    {
+        *dest = *src;
+        dest++;
+        src++;
+    }
+    *dest = '\0';
+
+    return dest;
+}
+
+int main(void)
+{
+    char src[20] = "Jay Ganesh";
+    char dest[20];
+
+    str_cpy(dest, src);
+
+    printf(dest);
+
+    return 0;
+}
