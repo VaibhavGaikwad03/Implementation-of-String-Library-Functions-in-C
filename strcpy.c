37f3c389c5191fbc09e55b8b2b12adfd98675077
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-01-31 12:37:10 +0530

    Add files via upload
    The "str_cpy" function copies the source string to the destination string.

---
+++ strcpy.c
@@ -0,0 +1,28 @@
+// The "str_cpy" function copies the source string to the destination string.
+
+#include <stdio.h>
+
+char *str_cpy(char *dest, const char *src)
+{
+    while (*src != '\0')
+    {
+        *dest = *src;
+        dest++;
+        src++;
+    }
+    *dest = '\0';
+
+    return dest;
+}
+
+int main(void)
+{
+    char src[20] = "Jay Ganesh";
+    char dest[20];
+
+    str_cpy(dest, src);
+
+    printf(dest);
+
+    return 0;
+}
