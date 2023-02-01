a8c057b034f724e791577774feb91dd76dbce67e
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-02-01 08:52:27 +0530

    Add files via upload
    The "str_n_cpy" function copies at the most n characters of a source string to destination string.

---
+++ strncpy.c
@@ -0,0 +1,28 @@
+// The "str_n_cpy" function copies at the most n characters of a source string to destination string.
+
+#include <stdio.h>
+
+char *str_n_cpy(char *dest, const char *src, int n)
+{
+    int iCnt = 0;
+
+    for (iCnt = 0; iCnt < n; iCnt++)
+    {
+        dest[iCnt] = src[iCnt];
+    }
+    dest[iCnt] = '\0';
+
+    return dest;
+}
+
+int main(void)
+{
+    char src[20] = "Marvellous";
+    char dest[20];
+
+    str_n_cpy(dest, src, 6);
+
+    printf(dest);
+
+    return 0;
+}
